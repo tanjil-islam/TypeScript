602278248b64d175cd8e77f2eb36ba1c06d4c73f
tanjil-islam <tanjil.islam040820@gmail.com>
2026-05-22 11:55:51 +0600

    Dynamic type or any type

---
+++ DynamicType.ts
@@ -0,0 +1,20 @@
+let a1: any[] = []
+a1.push(5)
+a1.push('Tanjil')
+
+console.log(a1)
+
+//object
+
+let b1: {
+  name: any
+  age: any
+  isMarried: any
+}
+
+b1 = {
+  name: 'Tanjil',
+  age: 25,
+  isMarried: false,
+}
+console.log(b1)
