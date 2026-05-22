628637a0910462053af560db4f0fc520b96ed854
tanjil-islam <tanjil.islam040820@gmail.com>
2026-05-22 10:17:17 +0600

    Basic type how it's work properly and where it's strict to convert variable type

---
+++ TypeBasic.js
@@ -0,0 +1,11 @@
+"use strict";
+let playerName = 'Tanjil';
+let age;
+age = 'sakib';
+age = 26;
+console.log(playerName);
+console.log(age);
+function mul(a, b) {
+    return a * b;
+}
+console.log(mul(5, 6));
---
+++ TypeBasic.ts
@@ -0,0 +1,27 @@
+let playerName = 'Tanjil'
+let age
+age = 'sakib'
+age = 26
+console.log(playerName)
+console.log(age)
+
+function mul(a: number, b: number) {
+  return a * b
+}
+console.log(mul(5, 6))
+
+//array
+
+let mixed = [1, 'Tanjil', true, {}]
+mixed.push({
+  name: 'sakib',
+})
+
+//object
+
+let person = {
+  name: 'Tanjil',
+  age: 26,
+  isMarried: false,
+}
+console.log(person.name)
